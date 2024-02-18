c4921f13ef885e193bb54c449cd527a43e6d7dd2
lookpat206 <prangfan@gmail.com>
2024-02-18 14:18:25 +0700

    edit

--- admin/user_add_save.php
+++ admin/user_add_save.php
@@ -5,7 +5,7 @@ $username = $_POST['username'];
 $u_name = $_POST['u_name'];
 $u_status = $_POST['u_status'];
 
-// exit($username . ' ' . $u_name.' '.$u_status);
+exit($username . ' ' . $u_name.' '.$u_status);
 
 user_add_save($username,$u_name,$u_status);
 
