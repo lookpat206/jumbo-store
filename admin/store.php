e648e3c80bd2b9c9c97d5d48c53e0020931ef2c7
lookpat206 <prangfan@gmail.com>
2025-03-18 14:46:14 +0700

    update

--- admin/store.php
+++ admin/store.php
@@ -4,17 +4,7 @@ include('_navbar.php');
 include('_sidebar_menu.php');
 include('_fn.php');
 
-
-//get mk_id by market.php
-$mk_id = $_GET['mk_id'];
-
-$result = fetch_mark_by_mkid($mk_id);
-$row = mysqli_fetch_assoc($result);
-$mk_name = $row['mk_name'];
-
 ?>
-
-
 
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -22,7 +12,7 @@ $mk_name = $row['mk_name'];
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>ข้อมูลร้านค้า</h1>
+                    <h1>ข้อมูลสินค้า</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -41,9 +31,9 @@ $mk_name = $row['mk_name'];
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">ข้อมูลร้านค้า</h3>
+                            <h3 class="card-title">ตารางข้อมูลสินค้า</h3>
                             <div class="card-tools">
-
+                                <a href="prod_add.php" class="btn btn-primary">เพิ่มข้อมูลสินค้า</a>
                             </div>
                         </div>
                         <!-- /.card-header -->
@@ -51,18 +41,18 @@ $mk_name = $row['mk_name'];
                             <table id="example1" class="table table-bordered table-striped">
 
                                 <thead>
-                                    <tr class="table-info">
-                                        <th width="5%">ลำดับ</th>
-                                        <th width="45%">ชื่อร้านค้า</th>
-
-                                        <th width="10%">แก้ไข</th>
-                                        <th width="10%">ลบ</th>
+                                    <tr>
+                                        <th style="width: 10%;">ลำดับ</th>
+                                        <th style="width: 50%;">ชื่อสินค้า</th>
+                                        <th style="width: 20%">เพิ่มร้านค้า</th>
+                                        <th style="width: 10%;">แก้ไข</th>
+                                        <th style="width: 10%;">ลบ</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
 
-                                    $result = fetch_supp();
+                                    $result = fetch_prod();
                                     if (mysqli_num_rows($result) > 0) {
                                         $i = 0;
                                         foreach ($result as $row) {
@@ -70,17 +60,17 @@ $mk_name = $row['mk_name'];
                                     ?>
                                             <tr>
                                                 <td><?= $i ?></td>
-                                                <td><?= $row['sp_name'] ?></td>
+                                                <td><?= $row['pd_n'] ?></td>
                                                 <td>
-                                                    <a class="btn btn-block btn-secondary" href="supp_add.php?mk_id=<?= $row['mk_id'] ?>">เพิ่มราคา</a>
+                                                    <a class="btn btn-block btn-secondary" href="supp.php?pd_id=<?= $row['pd_id'] ?>">เพิ่มร้านค้า</a>
                                                 </td>
                                                 <td>
-                                                    <a class="btn btn-warning btn-sm" href="supp.php?mk_id=<?= $row['mk_id'] ?>">
+                                                    <a class="btn btn-warning btn-sm" href="prod_edit.php?pd_id=<?= $row['pd_id'] ?>">
                                                         <i class="far fa-edit"></i>
                                                     </a>
                                                 </td>
                                                 <td>
-                                                    <a onClick="return confirm('Are you sure you want to delete?')" class="btn btn-danger btn-sm" href="spuu_delete.php?mk_id=<?= $row['sp_id'] ?>">
+                                                    <a onClick="return confirm('Are you sure you want to delete?')" class="btn btn-danger btn-sm" href="prod_delete.php?pd_id=<?= $row['pd_id'] ?>">
                                                         <i class="far fa-trash-alt"></i>
                                                     </a>
                                                 </td>
@@ -90,6 +80,8 @@ $mk_name = $row['mk_name'];
                                     } else {
                                         echo '<tr><td colspan="5">ไม่พบข้อมูล</td></tr>';
                                     }
+
+
 
 
 
