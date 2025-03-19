0c98ff815a1f2fb6499a32c4716b4d8a3bad491c
lookpat206 <prangfan@gmail.com>
2025-03-19 17:43:52 +0700

    update

--- admin/_sidebar_menu.php
+++ admin/_sidebar_menu.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
+  <a href="index.php" class="brand-link">
     <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">JumBo-Store</span>
   </a>
@@ -14,7 +14,7 @@
         <img src="dist/img/avatar2.png" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="" class="d-block">Admin</a>
+        <a href="index.php" class="d-block">Admin</a>
       </div>
     </div>
 
