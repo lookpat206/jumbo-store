ca5c485903d9582e699c1eb299bdd877c9ae4663
lookpat206 <prangfan@gmail.com>
2025-05-11 16:49:37 +0700

    add order in admin

---
+++ admin/order_save.php
@@ -0,0 +1,13 @@
+<?php
+include("_fn.php");
+
+// รับ   DATE  จาก   orders.php
+$c_id = $_POST["c_id"]; // ID-cust
+$od_day = $_POST["od_day"]; //order day
+$dv_day = $_POST["dv_day"]; //delivery day
+$dv_time = $_POST["dv_time"]; // delivery time
+$od_note = $_POST["od_note"]; //depatment
+
+//exit($c_id . $od_day . $dv_day . $od_note);
+
+order_add_save($c_id, $od_day, $dv_day, $dv_time, $od_note);
